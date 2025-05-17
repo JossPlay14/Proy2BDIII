74a0006b4bf32786d51695d384c83dda0ac0283d
JossPlay14 <153402738+JossPlay14@users.noreply.github.com>
2025-05-17 18:53:46 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,12 @@
-from backend.src import create_app  # Asegúrate que esta ruta sea correcta
+import sys
+from os.path import abspath, dirname
+
+# Añade el directorio backend al path
+sys.path.append(abspath(dirname(__file__)))
+
+from src.app import create_app  # Ajusta según tu estructura real
+
 app = create_app()
+
+if __name__ == "__main__":
+    app.run()
